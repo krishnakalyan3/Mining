cd71d1754205b437882f88d9316dbc252f37a677
didhavn coding <admin@didhavn.de>
2018-01-14 12:41:05 +0100

    added web

---
+++ web/js/main.js
@@ -0,0 +1,13 @@
+// chart.options.data[0].dataPoints.push(  { x: new Date(2014, 10, 01), y: 2289000 } );
+// chart.options.data[0].dataPoints.push(  { x: new Date(2014, 11, 01), y: 2345324 } );
+// chart.options.data[0].dataPoints.push(  { x: new Date(2015, 01, 01), y: 1231233 } );
+// chart.options.data[0].dataPoints.push(  { x: new Date(2015, 03, 01), y: 1253456 } );
+// chart.options.data[0].dataPoints.push(  { x: new Date(2015, 07, 01), y: 1123455 } );
+// chart.options.data[0].dataPoints.push(  { x: new Date(2015, 10, 01), y: 972000 } );
+//
+// chart.options.data[1].dataPoints.push(  { x: new Date(2014, 10, 01), y: 2389000 } );
+// chart.options.data[1].dataPoints.push(  { x: new Date(2014, 11, 01), y: 2445324 } );
+// chart.options.data[1].dataPoints.push(  { x: new Date(2015, 01, 01), y: 1331233 } );
+// chart.options.data[1].dataPoints.push(  { x: new Date(2015, 03, 01), y: 1353456 } );
+// chart.options.data[1].dataPoints.push(  { x: new Date(2015, 07, 01), y: 1223455 } );
+// chart.options.data[1].dataPoints.push(  { x: new Date(2015, 10, 01), y: 1202000 } );
